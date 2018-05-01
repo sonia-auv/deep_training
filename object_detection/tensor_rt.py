421a9ffd687cb06588c81c97ccaa21ae551e3bca
Martin Gauthier <gauthiermartin86@gmail.com>
2018-05-01 18:15:41 -0400

    testing convertion to tensorrt graph

---
+++ object_detection/tensor_rt.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
+import IPython
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+INPUT_TENSOR_NAME = 'image_tensor:0'
+
+OUPUT_TENSOR_NAMES  = [
+    'detection_boxes:0',
+    'detection_scores:0',
+    'detection_classes:0',
+    'num_detections:0',
+]
+
+MAX_BATCH_SIZE = 24
+WORKSPACE_SIZE = 4000000000
+PRECISION='FP32' #“FP32”, “FP16” or “INT8”
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.67)
+
+file_name = '/home/spark/Downloads/'
+
+with gfile.FastGFile(file_name,'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.reset_default_graph()
+    g=tf.Graph()
+
+    IPython.embed()
+
+
+    trt_graph = trt.create_inference_graph(
+        input_graph_def = frozen_graph_def,
+        outputs = output_node_name,
+        max_batch_size=batch_size,
+        max_workspace_size_bytes=workspace_size,
+        precision_mode=precision,
+        minimum_segment_size=3)
+
