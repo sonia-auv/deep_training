593523bd56b296393299839e314809de5adfbd26
Martin Gauthier <gauthiermartin86@gmail.com>
2018-05-01 21:51:12 -0400

    testing again

--- object_detection/test_rt.py
+++ object_detection/test_rt.py
@@ -1,66 +1,32 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import argparse
-import imghdr
-import json
 import os
-import sys
-import time
-import IPython
-
-import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.saved_model.python.saved_model import reader
-import tensorflow.contrib.tensorrt as trt
+from tensorflow.contrib import tensorrt as trt
 
+# Path to frozen Object Detection graph
+network_dir = "/home/spark/Models/frozen/frozen/mobilenet_v1/frozen_inference_graph.pb"
 
-def get_frozen_graph(graph_file):
-    """Read Frozen Graph file from disk."""
-    with tf.gfile.FastGFile(graph_file, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-    return graph_def
+print("Loading graph")
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    graph_def = tf.GraphDef()
+    with tf.gfile.GFile(network_dir, "rb") as fid:
+        serialized_graph = fid.read()
+        graph_def.ParseFromString(serialized_graph)
+        _ = tf.import_graph_def(graph_def, name='')
+print("Finished loading graph")
 
-
-
-def write_graph_to_file(graph_name, graph_def, output_dir):
-    """Write Frozen Graph file to disk."""
-    output_path = os.path.join(output_dir, graph_name)
-    with tf.gfile.GFile(output_path, "wb") as f:
-        f.write(graph_def.SerializeToString())
-
-
-def get_trt_graph(graph_name, graph_def, precision_mode, output_dir,
-output_node, batch_size=24, workspace_size=4000000000):
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.67)
-
-    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-
-    trt_graph = trt.create_inference_graph(
-        graph_def, output_node, max_batch_size=batch_size,
-        max_workspace_size_bytes=workspace_size,
-        precision_mode=precision_mode)
-    write_graph_to_file(graph_name, trt_graph, output_dir)
-
-
-def find_nodes(path_to_graph):
-   NODE_OPS = ['Placeholder','Identity']
-
-   gf = tf.GraphDef()
-   gf.ParseFromString(open(path_to_graph,'rb').read())
-   IPython.embed()
-   print([n.name for n in tf.get_default_graph().as_graph_def().node])
-   #print([n.name + '=>' +  n.op for n in gf.node if n.op in (NODE_OPS)])
-
-if __name__ == "__main__":
-    find_nodes('/home/spark/Models/frozen/frozen/mobilenet_v1/frozen_inference_graph.pb')
-    frozen_graph_def = get_frozen_graph('/home/spark/Models/frozen/frozen/mobilenet_v1/frozen_inference_graph.pb')
-
-    output_node_name  = [
-    'detection_boxes', 'detection_scores', 'detection_classes', 'num_detections',
-    ]
-
-    get_trt_graph('test',frozen_graph_def , "FP32", '/home/spark/',
-    output_node_name)
+trt_graph = trt.create_inference_graph(
+    input_graph_def=graph_def,
+    outputs=[
+        "detection_boxes",
+        "detection_classes",
+        "detection_scores",
+        "num_detections"
+    ],
+    max_batch_size=1,
+    max_workspace_size_bytes=1 << 32,
+    precision_mode="FP16",
+    minimum_segment_size=2
+)
+tf.reset_default_graph()
+tf.import_graph_def(graph_def=trt_graph)
